87775ac011e387fc2014da5d480fc1fa8bcd88c0
Sandip <suryawanshisandip2999@gmail.com>
2022-11-08 15:58:53 +0530

    added redux store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowswerRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowswerRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowswerRouter>
   </React.StrictMode>
-)
+);
