2310e0dc4962aec8d005aa31640287fb658125cd
Sandip <suryawanshisandip2999@gmail.com>
2022-11-08 22:18:50 +0530

    added chakra ui to project

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,12 +5,16 @@ import "./index.css";
 import { BrowswerRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowswerRouter>
       <Provider store={store}>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+
       </Provider>
     </BrowswerRouter>
   </React.StrictMode>
