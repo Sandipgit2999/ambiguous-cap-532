1342c1f4e0414ca9af5f235708a87bf48d39b021
Sandip <suryawanshisandip2999@gmail.com>
2022-11-09 10:20:24 +0530

    added browserrouter

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowswerRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowswerRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <ChakraProvider>
           <App />
         </ChakraProvider>
 
       </Provider>
-    </BrowswerRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
