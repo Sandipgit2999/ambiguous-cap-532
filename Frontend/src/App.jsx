4b1f036c53af2daaf7fa614c0f312f30e34842d0
AWMprabhat <prabhatbhardwajjuly18@gmail.com>
2022-11-10 23:14:24 +0530

    nav & footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 
-import './App.css'
+import Footer from './Components/footer/Footer'
+import Navbar from './Components/navbar/Navbar'
 import { Allroutes } from './Components/Routes/Allroutes'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
      <Allroutes/>
+     
        
     </div>
   )
