298e96f291d6e423132e1ce9cbb6d9a3c7d8016b
AWMprabhat <prabhatbhardwajjuly18@gmail.com>
2022-11-11 20:54:32 +0530

    navbar and Footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <div className="App">
-     <Allroutes/>
-     
-       
+     {/* <Allroutes/> */}
+     <Navbar/>
+<Footer/>
     </div>
   )
 }
---
+++ Frontend/src/Components/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+// import { Slider1 } from '../Home/Slider1'
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <Slider1/>
+//       <div>
+        
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default HomePage
