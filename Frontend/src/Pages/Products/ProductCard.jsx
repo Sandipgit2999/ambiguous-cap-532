579ad3b65ea99dc252ead15a0d2ee5e99adeec46
Abdul Rub <arub0419@gmail.com>
2022-11-14 13:06:30 +0530

    hove-effect

--- Frontend/src/Pages/Products/ProductCard.jsx
+++ Frontend/src/Pages/Products/ProductCard.jsx
@@ -1,8 +1,9 @@
 import { Box, Grid, Text, Image, Flex } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { BsHandbag } from "react-icons/bs";
-import { AiOutlineHeart } from "react-icons/ai"
+import { AiOutlineHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import styles from "./ProductCard.module.css"
 
 export const ProductCard = ({ item }) => {
   const [visible, setVisible] = useState(false);
@@ -12,25 +13,30 @@ export const ProductCard = ({ item }) => {
       position={"relative"}
       // border={"1px solid red"}
       onMouseEnter={() => {
+        console.log("yes")
         setVisible(true);
       }}
       onMouseLeave={() => {
+        console.log("no")
         setVisible(false);
       }}
     >
-      <Link to={`/products/${item._id}`}><Box>
-        <Image h={380} src={item.image_link} w={"100%"} />
-        <Text mt={2} fontWeight="bold" textTransform={"uppercase"}>
-          {item.brand}
-        </Text>
-        <Text>{item.name}</Text>
-        <Text fontWeight="bold" fontSize={13}>
-          Rs: {item.price}
-        </Text>
-        <Text>{item.id}</Text>
-      </Box></Link>
+      <Link to={`/products/${item._id}`}>
+        <Box>
+          <Image h={380} src={item.image_link} w={"100%"} />
+          <Text mt={2} fontWeight="bold" textTransform={"uppercase"}>
+            {item.brand}
+          </Text>
+          <Text>{item.name}</Text>
+          <Text fontWeight="bold" fontSize={13}>
+            Rs: {item.price}
+          </Text>
+          <Text>{item.id}</Text>
+        </Box>
+      </Link>
       <Box
-        className={visible ? "hov" : "nonhover"}
+        // className={visible ? "hov" : "nonhover"}
+        className={visible?styles.hov:styles.nonhover}
         // bg="#aaf0d1 "
         bg="rgb(22, 236, 208)"
         opacity={1}
@@ -46,7 +52,7 @@ export const ProductCard = ({ item }) => {
             <BsHandbag color="white" fontSize={"50"} />
             <Text color="white">SHOP NOW</Text>
           </Box>
-          <Box >
+          <Box>
             <AiOutlineHeart color="white" fontSize={"55"} />
             <Text color="white">FAVOURITE</Text>
           </Box>
